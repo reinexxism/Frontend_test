1f1a2ebdb3d5a7e8c15b0d05940c455d683c50db
reinexxism <lucy0805@naver.com>
2025-03-30 15:27:47 +0900

    상단 네비게이션바 스타일링 완료

--- src/components/CategoryTab.jsx
+++ src/components/CategoryTab.jsx
@@ -1,16 +1,15 @@
 import React from "react";
-import { CategoryTabContainer } from "../styles/CategoryTabStyle";
-import { Link } from "react-router-dom";
+import { CategoryTabContainer, StyledLink } from "../styles/CategoryTabStyle";
 
 export default function CategoryTab() {
   return (
     <CategoryTabContainer>
-      <Link to="/">차트</Link>
-      <Link to="/whookpage">Whook</Link>
-      <Link to="/eventpage">이벤트</Link>
-      <Link to="/newspage">뉴스</Link>
-      <Link to="/storepage">스토어</Link>
-      <Link to="/chargepage">충전소</Link>
+      <StyledLink to="/">차트</StyledLink>
+      <StyledLink to="/whookpage">Whook</StyledLink>
+      <StyledLink to="/eventpage">이벤트</StyledLink>
+      <StyledLink to="/newspage">뉴스</StyledLink>
+      <StyledLink to="/storepage">스토어</StyledLink>
+      <StyledLink to="/chargepage">충전소</StyledLink>
     </CategoryTabContainer>
   );
 }
--- src/styles/CategoryTabStyle.jsx
+++ src/styles/CategoryTabStyle.jsx
@@ -1,13 +1,28 @@
 import styled from "styled-components";
+import { NavLink } from "react-router-dom";
 
 export const CategoryTabContainer = styled.div`
   width: 425px;
   height: 100%;
   background-color: #ffb0b2;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
 `;
 
 export const ContentContainer = styled.div`
   width: 425px;
   height: 650px;
   background-color: #421e1e;
+`;
+
+export const StyledLink = styled(NavLink)`
+  text-decoration: none;
+  color: black;
+  font-size: 18px;
+  font-weight: bold;
+
+  &.active {
+    color: #fff;
+  }
 `;
